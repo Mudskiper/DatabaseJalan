4d212a2cd88ec26ee8f6f4113c62c1c7862f5e73
Mudskiper <aureonff@gmail.com>
2024-09-19 05:46:40 +0700

    Update stopload.js

--- js/stopload.js
+++ js/stopload.js
@@ -2,7 +2,7 @@ function stopPageLoad() {
                 setTimeout(function() {
                     window.stop(); // Hentikan pemuatan halaman
                     alert("Persembahan dari Tim PADAMU NEGERI Bidang Bina Marga Dinas PUPR | Email : binamargatjt@gmail.com");
-                }, 60000); // 60000 ms = 60 detik
+                }, 40000); // 40000 ms = 40 detik
             }
 
             // Eksekusi fungsi stopPageLoad paling awal
