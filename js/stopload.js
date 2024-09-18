fd9251cc5631814423cd875593409a883fb097a6
Mudskiper <aureonff@gmail.com>
2024-09-18 19:16:02 +0700

    Add files via upload

--- js/stopload.js
+++ js/stopload.js
@@ -2,7 +2,7 @@ function stopPageLoad() {
                 setTimeout(function() {
                     window.stop(); // Hentikan pemuatan halaman
                     alert("Persembahan dari Tim PADAMU NEGERI Bidang Bina Marga Dinas PUPR | Email : binamargatjt@gmail.com");
-                }, 10000); // 10000 ms = 10 detik
+                }, 20000); // 20000 ms = 20 detik
             }
 
             // Eksekusi fungsi stopPageLoad paling awal
