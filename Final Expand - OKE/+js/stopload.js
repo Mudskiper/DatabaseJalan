7c5789ba3e17834a328c0e97a514ad68175ab192
Mudskiper <aureonff@gmail.com>
2024-09-30 23:00:17 +0700

    Delete Final Expand - OKE directory

--- Final Expand - OKE/+js/stopload.js
+++
@@ -1,9 +0,0 @@
-function stopPageLoad() {
-    setTimeout(function () {
-        window.stop(); // Hentikan pemuatan halaman
-        alert("Persembahan dari Tim PADAMU NEGERI Bidang Bina Marga Dinas PUPR | Email : binamargatjt@gmail.com");
-    }, 30000); // 30000 ms = 30 detik
-}
-
-// Eksekusi fungsi stopPageLoad paling awal
-stopPageLoad();
